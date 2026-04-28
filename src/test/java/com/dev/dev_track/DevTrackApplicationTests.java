bec981abd9c96b45b6b1d0df7d1eb8c9b52a96fc
DewmithMihisara <mihisaralokuhewage@gmail.com>
2026-04-28 07:10:26 +0530

    first commit

---
+++ src/test/java/com/dev/dev_track/DevTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.dev_track;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
