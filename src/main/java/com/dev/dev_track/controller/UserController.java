1598ee68debdcfbdb4e2a60350e874e884a6c5cf
DewmithMihisara <mihisaralokuhewage@gmail.com>
2026-04-28 09:26:24 +0530

    add swagger

--- src/main/java/com/dev/dev_track/controller/UserController.java
+++ src/main/java/com/dev/dev_track/controller/UserController.java
@@ -3,16 +3,16 @@ package com.dev.dev_track.controller;
 import com.dev.dev_track.dto.ResponseDto;
 import com.dev.dev_track.dto.UserDto;
 import com.dev.dev_track.service.UserService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-@CrossOrigin("*")
+@RequestMapping("/user")
+@CrossOrigin(origins = "*")
+@Tag(name = "user Controller", description = "user related operations...")
 public class UserController {
     private final UserService userService;
 
