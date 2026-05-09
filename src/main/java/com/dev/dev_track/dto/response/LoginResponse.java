1bf6227697680ec7b7d6dfafcd41a59c19b89f01
DewmithMihisara <mihisaralokuhewage@gmail.com>
2026-05-09 14:20:32 +0530

    feat: add ApiResponse wrapper, RegisterRequest, LoginRequest, LoginResponse DTOs

---
+++ src/main/java/com/dev/dev_track/dto/response/LoginResponse.java
@@ -0,0 +1,18 @@
+package com.dev.dev_track.dto.response;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class LoginResponse {
+
+    private String accessToken;
+    private String refreshToken;
+    private String tokenType;
+    private Long userId;
+    private String email;
+    private String username;
+}
